ff37ca07fa13a2d4d6a7aedca287a91ca7a5c5c9
jjacky <i.am.jack.mail@gmail.com>
2012-03-19 13:16:20 +0100

    added GPL headers

--- main.h
+++ main.h
@@ -1,3 +1,24 @@
+/**
+ * molt - Copyright (C) 2012 Olivier Brunel
+ *
+ * main.h
+ * Copyright (C) 2012 Olivier Brunel <i.am.jack.mail@gmail.com>
+ * 
+ * This file is part of molt.
+ *
+ * molt is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * molt is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * molt. If not, see http://www.gnu.org/licenses/
+ */
 
 #ifndef MAIN_H
 #define	MAIN_H
